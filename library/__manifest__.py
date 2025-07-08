14fa91c3e67f40c0705a3b0466ed32fa5703a825
ValouReuz <valentin.messiaen@reuz.com>
2025-07-08 11:57:21 +0200

    Fix actions

--- library/__manifest__.py
+++ library/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Une librairie',
-    'version': '18.0.1.0',
+    'version': '18.0.1.1',
     'summary': 'Un exemple de librairie',
     'description': "Une librairie",
     'author': 'Re-uz distribution',
