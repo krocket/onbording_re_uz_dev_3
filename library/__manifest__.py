3eb7bf210b1bb799fb20fdce41342603d9392b74
ValouReuz <valentin.messiaen@reuz.com>
2025-07-08 11:12:47 +0200

    Added model library

---
+++ library/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Une librairie',
+    'version': '18.0.1',
+    'summary': 'Un exemple de librairie',
+    'description': "Une librairie",
+    'author': 'Re-uz distribution',
+    'category': 'library',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml'
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
