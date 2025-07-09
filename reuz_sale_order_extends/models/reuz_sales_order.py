230da1a04a3070d60b0b8f71efb52ac1d5bd1d8c
ValouReuz <valentin.messiaen@reuz.com>
2025-07-09 15:26:21 +0200

    Added integer attributes

--- reuz_sale_order_extends/models/reuz_sales_order.py
+++ reuz_sale_order_extends/models/reuz_sales_order.py
@@ -10,7 +10,7 @@ class ReuzSalesOrder(models.Model):
   silver_reference = fields.Char(string='Silver reference')
   planned_date = fields.Date(string='Date de plannification')
   format_product = fields.Char(string='Format produit')
-  volume_product = fields.Int(string='Volume produit')
+  volume_product = fields.Integer(string='Volume produit', default=0)
   base_product = fields.Char(string='Base produit')
   color_product = fields.Char(string='Color produit')
   technology_product = fields.Char(string='Technology produit')
