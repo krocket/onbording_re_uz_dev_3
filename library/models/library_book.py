caee2c82c405ed1576f5f4724d369eef307aff19
ValouReuz <valentin.messiaen@reuz.com>
2025-07-09 10:08:44 +0200

    Added button check

--- library/models/library_book.py
+++ library/models/library_book.py
@@ -1,4 +1,9 @@
-from odoo import fields, models
+from odoo import fields, models, UserError
+from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
 
 class Book(models.Model):
   _name = 'library.book'
@@ -11,3 +16,25 @@ class Book(models.Model):
   publisher_id = fields.Many2one('res.partner', string='Publisher')
   author_ids = fields.Many2many('res.partner', string='Authors')
   category_ids = fields.Many2many('library.book.category', string='Categories')
+
+  def _check_isbn(self):
+    self.ensure_one()
+    digits = [int(x) for x in self.isbn if x.isdigit()]
+    if len(digits) == 13:
+      return True
+    else : 
+      return False
+
+  def button_check_isbn(self):
+    _logger.info(self)
+    for book in self:
+      if not book.isbn:
+        raise UserError(
+          'Please provide an ISBN13 for %s' % book.name)
+      if book.isbn and not book._check_isbn():
+        raise UserError(
+          '%s is an invalid ISBN' % book.isbn)
+      return True
+    
+  def button_your_are_best(self):
+    raise UserError("T'es le meilleurs")
